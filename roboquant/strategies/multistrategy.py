aba3970a83956b8e2509bf946efbd3426688ed79
Peter Dekkers <peter@neurallayer.com>
2024-02-21 13:44:23 +0100

    simplified mutli strat

--- roboquant/strategies/multistrategy.py
+++ roboquant/strategies/multistrategy.py
@@ -8,18 +8,13 @@ class MultiStrategy(Strategy):
     """Combine one or more strategies. The MultiStrategy provides additional control on how to handle conflicting
     signals for the same symbols:
 
-    - avg: average the signal ratings, and optionally apply weights based on the stratetegy that generated the rating
     - first: in case of multiple signals for a symbol, the first strategy wins
     - last:  in case of multiple signals for a symbol, the last strategy wins. This is also the default policy
     """
 
-    def __init__(
-        self, *strategies: Strategy, policy: Literal["last", "first"] = "last", weights: list[float] | None = None
-    ):
+    def __init__(self, *strategies: Strategy, policy: Literal["last", "first"] = "last"):
         self.strategies = list(strategies)
         self.policy = policy
-        self.weights = weights or [1.0] * len(strategies)
-        assert len(self.weights) == len(self.strategies), "weights and strategies should be of equal lenght"
 
     def create_signals(self, event: Event) -> dict[str, Signal]:
         all_signals: list[dict[str, Signal]] = []
