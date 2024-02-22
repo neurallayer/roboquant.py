55b0f2143722bf33fa2a2a2dc5d4fcf674229656
Peter Dekkers <peter@neurallayer.com>
2024-02-22 09:53:03 +0100

    more tests for signal

--- tests/unit/test_signal.py
+++ tests/unit/test_signal.py
@@ -8,17 +8,24 @@ class TestSignal(unittest.TestCase):
         s = BUY
         self.assertEqual(1.0, s.rating)
         self.assertTrue(s.is_buy)
+        self.assertFalse(s.is_sell)
+        self.assertTrue(s.is_entry)
+        self.assertTrue(s.is_exit)
         self.assertTrue(SignalType.ENTRY in s.type)
         self.assertTrue(SignalType.EXIT in s.type)
 
+    def test_signal_equal(self):
         x = SELL
         y = Signal(-1.0, SignalType.BOTH)
         self.assertEqual(x, y)
+        self.assertEqual(BUY, BUY)
+        self.assertNotEqual(BUY, SELL)
 
+    def test_signal_rating(self):
         s = Signal(0.5, SignalType.ENTRY)
         self.assertEqual(0.5, s.rating)
-        self.assertTrue(SignalType.ENTRY in s.type)
-        self.assertFalse(SignalType.EXIT in s.type)
+        self.assertTrue(s.is_entry)
+        self.assertFalse(s.is_exit)
 
 
 if __name__ == "__main__":
