8cd35503d36dcb9b4e697c98e06acd1ca5e37baf
Peter Dekkers <peter@neurallayer.com>
2024-03-04 14:58:11 +0100

    removed some lines

--- tests/samples/dataframe.py
+++ tests/samples/dataframe.py
@@ -7,10 +7,8 @@ if __name__ == "__main__":
     warnings.simplefilter(action="ignore", category=FutureWarning)
 
     # %%
-    feed = rq.feeds.YahooFeed("JPM", "IBM", "F", start_date="2010-01-01")
+    feed = rq.feeds.YahooFeed("IBM", start_date="2020-01-01")
     ohlcv = rq.feeds.feedutil.get_symbol_ohlcv(feed, "IBM")
 
     # %%
     pd.DataFrame(ohlcv).set_index("Date")
-
-# %%
