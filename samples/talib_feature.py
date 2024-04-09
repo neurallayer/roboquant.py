395ef20d0555c9e4f2f89e57a3b2ac6be65e6edd
Peter Dekkers <peter@neurallayer.com>
2024-04-09 14:17:24 +0200

    sample improvement

--- samples/talib_feature.py
+++ samples/talib_feature.py
@@ -4,16 +4,24 @@ import roboquant as rq
 from roboquant.ml.features import TaFeature
 
 # %%
-class MyFeature(TaFeature):
-    """Example using talib to create a combined RSI/BollingerBand strategy"""
+class RSIFeature(TaFeature):
+    """Example using talib to create a RIS feature"""
 
 
-    def __init__(self, *symbols: str) -> None:
-        super().__init__(*symbols, history_size=10)
+    def __init__(self, *symbols: str, timeperiod=10) -> None:
+        self.timeperiod = timeperiod
+        super().__init__(*symbols, history_size=timeperiod+1)
 
     def _calc(self, symbol, ohlcv) -> float:
         close = ohlcv.close()
-        return ta.RSI(close, timeperiod=9)  # type: ignore
+        return ta.RSI(close, timeperiod=self.timeperiod)  # type: ignore
 
 # %%
-feature = MyFeature()
+feed = rq.feeds.YahooFeed("IBM", "AAPL", start_date="2024-01-01", end_date="2024-02-01")
+feature = RSIFeature("IBM", "AAPL", timeperiod=15)
+account = rq.Account()
+channel = feed.play_background()
+
+while evt := channel.get():
+    result = feature.calc(evt, account)
+    print(result)
