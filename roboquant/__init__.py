ebb58a798d69e925de53db4b1e8c977896dd1b58
Peter Dekkers <peter@neurallayer.com>
2024-12-15 18:04:59 +0100

    updated version

--- roboquant/__init__.py
+++ roboquant/__init__.py
@@ -2,7 +2,7 @@
 The `roboquant` package contains the `run` method and shared classes like `Account`, `Asset` and `Event`.
 """
 
-__version__ = "0.9.6"
+__version__ = "0.9.7"
 
 import logging
 
--- roboquant/strategies/__init__.py
+++ roboquant/strategies/__init__.py
@@ -3,3 +3,5 @@ from .tastrategy import TaStrategy
 from .emacrossover import EMACrossover
 from .multistrategy import MultiStrategy
 from .strategy import Strategy
+
+__all__ = ["Strategy", "MultiStrategy","EMACrossover", "TaStrategy", "NumpyBuffer", "OHLCVBuffer"]
