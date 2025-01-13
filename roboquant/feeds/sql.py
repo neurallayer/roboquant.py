115839bd2ab8c6a7b46e95eaa154800db7bebb62
Peter Dekkers <peter@neurallayer.com>
2025-01-13 11:53:56 +0100

    use with for sql feed

--- roboquant/feeds/sql.py
+++ roboquant/feeds/sql.py
@@ -43,32 +43,32 @@ class SQLFeed(Feed):
         return os.path.exists(self.db_file)
 
     def create_index(self):
-        con = sqlite3.connect(self.db_file)
-        con.execute(SQLFeed._sql_create_index)
-        con.commit()
+        with sqlite3.connect(self.db_file) as con:
+            con.execute(SQLFeed._sql_create_index)
+            con.commit()
 
     def number_items(self):
-        con = sqlite3.connect(self.db_file)
-        result = con.execute(SQLFeed._sql_count_items).fetchall()
-        con.commit()
-        row = result[0]
-        return row[0]
+        with sqlite3.connect(self.db_file) as con:
+            result = con.execute(SQLFeed._sql_count_items).fetchall()
+            con.commit()
+            row = result[0]
+            return row[0]
 
     def timeframe(self):
-        con = sqlite3.connect(self.db_file)
-        result = con.execute(SQLFeed._sql_select_timeframe).fetchall()
-        con.commit()
-        row = result[0]
-        if row[0]:
-            return Timeframe.fromisoformat(row[0], row[1], True)
-        return Timeframe.EMPTY
+        with sqlite3.connect(self.db_file) as con:
+            result = con.execute(SQLFeed._sql_select_timeframe).fetchall()
+            con.commit()
+            row = result[0]
+            if row[0]:
+                return Timeframe.fromisoformat(row[0], row[1], True)
+            return Timeframe.EMPTY
 
     def assets(self):
-        con = sqlite3.connect(self.db_file)
-        result = con.execute(SQLFeed._sql_select_assets).fetchall()
-        con.commit()
-        assets = {Asset.deserialize(columns[0]) for columns in result}
-        return assets
+        with sqlite3.connect(self.db_file) as con:
+            result = con.execute(SQLFeed._sql_select_assets).fetchall()
+            con.commit()
+            assets = {Asset.deserialize(columns[0]) for columns in result}
+            return assets
 
     def get_item(self, row) -> PriceItem:
         if self.is_bar:
@@ -83,30 +83,30 @@ class SQLFeed(Feed):
         return Quote(asset, array("f", data))
 
     def play(self, channel: EventChannel):
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
-        t_old = ""
-        items = []
-        tf = channel.timeframe
-        result = (
-            cur.execute(SQLFeed._sql_select_by_date, [tf.start.isoformat(), tf.end.isoformat()])
-            if tf
-            else cur.execute(SQLFeed._sql_select_all)
-        )
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
+            t_old = ""
+            items = []
+            tf = channel.timeframe
+            result = (
+                cur.execute(SQLFeed._sql_select_by_date, [tf.start.isoformat(), tf.end.isoformat()])
+                if tf
+                else cur.execute(SQLFeed._sql_select_all)
+            )
 
-        for row in result:
-            t = row[0]
-            assert t >= t_old, f"{t} t_old"
-            if t != t_old:
-                if items:
-                    dt = datetime.fromisoformat(t_old)
-                    event = Event(dt, items)
-                    channel.put(event)
-                    items = []
-                t_old = t
+            for row in result:
+                t = row[0]
+                assert t >= t_old, f"{t} t_old"
+                if t != t_old:
+                    if items:
+                        dt = datetime.fromisoformat(t_old)
+                        event = Event(dt, items)
+                        channel.put(event)
+                        items = []
+                    t_old = t
 
-            item = self.get_item(row)
-            items.append(item)
+                item = self.get_item(row)
+                items.append(item)
 
         # are there leftovers
         if items:
@@ -119,38 +119,37 @@ class SQLFeed(Feed):
     def record(self, feed: Feed, timeframe=None, append=False, batch_size=10_000):
         """Record another feed into this SQLite database.
         It only supports Bars and Quotes"""
-        con = sqlite3.connect(self.db_file)
-        cur = con.cursor()
+        with sqlite3.connect(self.db_file) as con:
+            cur = con.cursor()
 
-        create_sql = SQLFeed._sql_create_bar_table if self.is_bar else SQLFeed._sql_create_quote_table
-        insert_sql = SQLFeed._sql_insert_bar if self.is_bar else SQLFeed._sql_insert_quote
+            create_sql = SQLFeed._sql_create_bar_table if self.is_bar else SQLFeed._sql_create_quote_table
+            insert_sql = SQLFeed._sql_insert_bar if self.is_bar else SQLFeed._sql_insert_quote
 
-        if not append:
-            cur.execute(SQLFeed._sql_drop_table)
+            if not append:
+                cur.execute(SQLFeed._sql_drop_table)
 
-        cur.execute(create_sql)
-        price_type = self.price_type
+            cur.execute(create_sql)
+            price_type = self.price_type
 
-        channel = feed.play_background(timeframe)
-        data = []
-        while event := channel.get():
-            t = event.time
-            for item in event.items:
-                if isinstance(item, Bar) and price_type == "bar":
-                    elem = (t.isoformat(), item.asset.serialize(), *item.ohlcv, item.frequency)
-                    data.append(elem)
-                elif isinstance(item, Quote) and price_type == "quote":
-                    elem = (t.isoformat(), item.asset.serialize(), *item.data)
-                    data.append(elem)
-            if len(data) >= batch_size:
+            channel = feed.play_background(timeframe)
+            data = []
+            while event := channel.get():
+                t = event.time
+                for item in event.items:
+                    if isinstance(item, Bar) and price_type == "bar":
+                        elem = (t.isoformat(), item.asset.serialize(), *item.ohlcv, item.frequency)
+                        data.append(elem)
+                    elif isinstance(item, Quote) and price_type == "quote":
+                        elem = (t.isoformat(), item.asset.serialize(), *item.data)
+                        data.append(elem)
+                if len(data) >= batch_size:
+                    cur.executemany(insert_sql, data)
+
+            if data:
                 cur.executemany(insert_sql, data)
 
-        if data:
-            cur.executemany(insert_sql, data)
-
-        con.commit()
-        con.close()
-        logger.info("inserted rows=%s", len(data))
+            con.commit()
+            logger.info("inserted rows=%s", len(data))
 
     def __repr__(self) -> str:
         return f"SQLFeed(timeframe={self.timeframe()} items={self.number_items()} assets={len(self.assets())})"
