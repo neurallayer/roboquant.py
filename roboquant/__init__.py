c6aa5466b6ec228ebf1adff4b324cc2117eebcbe
Peter Dekkers <peter@neurallayer.com>
2026-03-20 08:37:01 +0100

    updated version

--- roboquant/__init__.py
+++ roboquant/__init__.py
@@ -3,7 +3,7 @@ The `roboquant` package contains the `run` method and a number of shared classes
 like `Account`, `Asset` and `Event`.
 """
 
-__version__ = "1.9.12"
+__version__ = "2.0.0"
 
 import logging
 
