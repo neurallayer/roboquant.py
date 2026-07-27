788600d67a9a31629cfd1549bb05ee970d092dda
Peter Dekkers <peter@neurallayer.com>
2026-07-27 08:53:39 +0200

    add small info function

--- roboquant/__init__.py
+++ roboquant/__init__.py
@@ -21,8 +21,34 @@ from .timeframe import Timeframe, utcnow
 from .timeseries import TimeSeries
 
 from importlib.metadata import version
+import platform
 __version__ = version("roboquant")
 
+def info():
+
+    msg = r"""             _______
+            | $   $ |         Roboquant v__version__
+            |   o   |         Python v__python__
+            |_[___]_|         __system__
+        ___ ___|_|___ ___
+       ()___)       ()___)
+      /  / |         | \  \
+     (___) |_________| (___)
+      | |   __/___\__   | |
+      /_\  |_________|  /_\
+     // \\  |||   |||  // \\
+     \\ //  |||   |||  \\ //
+           ()__) ()__)
+           ///     \\\
+        __///_     _\\\__
+       |______|   |______|"""
+
+    msg = msg.replace("__version__", __version__)
+    msg = msg.replace("__python__", platform.python_version())
+    uname = platform.uname()
+    sys_info = f"{uname.system} v{uname.release}"
+    msg = msg.replace("__system__", sys_info)
+    print(msg, flush=True)
 
 __all__ = [
     "brokers",
