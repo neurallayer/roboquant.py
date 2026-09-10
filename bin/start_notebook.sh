0a403639207ee50b63d221f455732c8b8f8567de
Peter Dekkers <peter@neurallayer.com>
2026-09-10 10:18:11 +0200

    better script

--- bin/start_notebook.sh
+++ bin/start_notebook.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 [[ ! -f "LICENSE" ]] && echo "run the script from the project root directory like this: ./bin/create_docs.sh" && exit 1
 
+mkdir -p scratch
 cd scratch
-uv run --with jupyter jupyter lab test.ipynb
+uv run --with jupyter jupyter notebook
